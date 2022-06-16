acf691c0f4d77b5727e76d27b2750c14657656a2
litundebata <litundebata@gmail.com>
2022-06-16 13:21:44 +0530

    Changes made to the homepage

---
+++ header-firstHalf.js
@@ -0,0 +1,17 @@
+  
+      var swiper = new Swiper(".mySwiper", {
+        slidesPerView: 6,
+        spaceBetween: 15,
+        slidesPerGroup: 6,
+        loop: true,
+        loopFillGroupWithBlank: true,
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+      });
+ 
