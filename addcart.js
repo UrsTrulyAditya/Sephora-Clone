1c002e3aaa56146258fa7bd2403762053d2a6048
Faizankhan99 <usmani.faizan98@gmail.com>
2022-06-18 16:00:58 +0530

    final

--- addcart.js
+++ addcart.js
@@ -5,21 +5,60 @@ display(ProductData);
 
 
 function display(ProductData) {
-  ProductData.forEach(function (elem) {
-    let box = document.createElement("div");
 
+  let sum=0
+  let total=0
+  let dis=0
+
+  ProductData.forEach(function (elem,index) {
+
+sum=sum+Number(elem.price)
+total=sum
+
+document.querySelector("#total").innerText="$ "+ total
+
+document.querySelector("#bt").addEventListener("click",function(){
+   let voucher=document.querySelector("#voucher").value;
+// console.log("inner");
+if(voucher==="masai30"){
+  let per=total*10/100;
+  dis=total-per
+  // console.log(dis)
+  // alert("you got discount")
+  document.querySelector("#total").innerText=dis;
+  total=dis
+}
+
+})
+
+    let box = document.createElement("div");
     let image = document.createElement("img");
     image.setAttribute("src", elem.Image);
 
-    let des = document.createElement("h3");
+    let des = document.createElement("h4");
     des.innerText = elem.des;
 
-    let name = document.createElement("h4");
+    let name = document.createElement("h5");
     name.innerText = elem.name;
 
-    let price = document.createElement("p");
-    price.innerText = elem.price;
-    box.append(image, des, name, price);
+    let price=document.createElement("p");
+    price.innerText = "$"+elem.price;
+
+    let btn=document.createElement("button");
+    btn.innerText = "Remove"
+    btn.addEventListener("click", function() {
+    remove (index)
+    });
+
+    box.append(image, name, des, price,btn);
     document.querySelector(".bthree").append(box);
   });
+
+}
+
+
+function remove(index) {
+  ProductData.splice(index,1) 
+  localStorage.setItem("cart-page",JSON.stringify(ProductData))
+  window.location.reload();
 }
